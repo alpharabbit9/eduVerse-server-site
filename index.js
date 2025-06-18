834e39c068d64dd004a934e8fab335f992659c52
alpharabbit9 <alpharabbit74@gmail.com>
2025-06-19 01:38:13 +0600

    added confirmedCourse . update , delete , approve , reject API

--- index.js
+++ index.js
@@ -32,6 +32,7 @@ async function run() {
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
 
     const courseCollection = client.db('eduerseDB').collection('courses');
+    const ConfirmedCourseCollection = client.db('eduerseDB').collection('confirmedCourse');
     const userCollection = client.db('eduerseDB').collection('users');
     const teacherRequestCollection = client.db('eduerseDB').collection('teacherRequest');
 
@@ -49,7 +50,7 @@ async function run() {
 
       const user = req.body;
 
-      console.log(user)
+      // console.log(user)
 
       const query = { email: user.email }
 
@@ -72,21 +73,21 @@ async function run() {
       res.send(result)
     })
 
-    app.patch('/users/teacher/:email' , async(req,res) =>{
+    app.patch('/users/teacher/:email', async (req, res) => {
 
       const email = req.params.email;
 
-      const filter = {email : email}
+      const filter = { email: email }
 
       const updatedDoc = {
-        $set :
+        $set:
         {
 
-          role : 'teacher' 
+          role: 'teacher'
         }
       }
 
-      const result = await userCollection.updateOne(filter , updatedDoc);
+      const result = await userCollection.updateOne(filter, updatedDoc);
 
       res.send(result)
 
@@ -94,20 +95,20 @@ async function run() {
 
     })
 
-    app.patch('/users/reject/:email' , async(req,res) =>{
+    app.patch('/users/reject/:email', async (req, res) => {
 
       const email = req.params.email;
 
-      const filter = {email:email}
+      const filter = { email: email }
 
       const updatedDoc = {
 
-        $set : {
-          role : "student"
+        $set: {
+          role: "student"
         }
       }
 
-      const result = await userCollection.updateOne(filter , updatedDoc);
+      const result = await userCollection.updateOne(filter, updatedDoc);
 
       res.send(result)
     })
@@ -137,22 +138,21 @@ async function run() {
 
     // ! Teacher Related API
 
-    app.get('/teacherRequest' , async (req ,res) =>{
+    app.get('/teacherRequest', async (req, res) => {
 
       const result = await teacherRequestCollection.find().toArray()
       res.send(result)
     })
 
-    app.post('/teacherRequest' , async (req,res) =>{
+    app.post('/teacherRequest', async (req, res) => {
       const request = req.body;
 
-      const query = {email : req.body.teacher_email};
+      const query = { email: req.body.teacher_email };
 
       const isExist = await teacherRequestCollection.findOne(query)
 
-      if(isExist)
-      {
-         return res.status(400).send({ message: "Request already exists" });
+      if (isExist) {
+        return res.status(400).send({ message: "Request already exists" });
       }
 
       const result = await teacherRequestCollection.insertOne(request);
@@ -160,47 +160,47 @@ async function run() {
       res.send(result)
     })
 
-    app.patch('/teacherRequest/:id', async(req,res) =>{
+    app.patch('/teacherRequest/:id', async (req, res) => {
 
-      const id = req.params.id ;
+      const id = req.params.id;
 
-      const filter = {_id : new ObjectId(id)}
+      const filter = { _id: new ObjectId(id) }
 
       const updatedDoc = {
-        $set : 
+        $set:
         {
-          status : 'Approved'
+          status: 'Approved'
         }
       }
 
-      const result = await teacherRequestCollection.updateOne(filter,updatedDoc);
+      const result = await teacherRequestCollection.updateOne(filter, updatedDoc);
 
       res.send(result)
     })
 
-    app.patch('/teacherRequest/reject/:id' , async(req,res) =>{
+    app.patch('/teacherRequest/reject/:id', async (req, res) => {
 
-      const id = req.params.id ;
+      const id = req.params.id;
 
-      const filter = {_id : new ObjectId(id)}
+      const filter = { _id: new ObjectId(id) }
 
       const updatedDoc = {
 
-        $set : {
+        $set: {
 
-          status : "rejected"
+          status: "rejected"
 
         }
-        
+
       }
 
-      const result = await teacherRequestCollection.updateOne(filter,updatedDoc)
+      const result = await teacherRequestCollection.updateOne(filter, updatedDoc)
 
       res.send(result)
     })
 
 
-    
+
 
 
     //! Course Related API
@@ -210,11 +210,110 @@ async function run() {
       res.send(result)
     })
 
-    app.post('/courses' , async(req,res) =>{
+    // ? Courses search by email 
 
-      const course = req.body ;
+    app.get('/courses/:email', async (req, res) => {
 
-      const result = await  courseCollection.insertOne(course)
+      const email = req.params.email;
+
+      const query = { email: email }
+
+      const result = await courseCollection.find(query).toArray();
+
+      res.send(result)
+    })
+
+    app.put('/courses/:id', async (req, res) => {
+      const id = req.params.id;
+      const updatedCourse = req.body;
+
+      const filter = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $set: updatedCourse
+      };
+
+      const result = await courseCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    });
+
+
+    app.post('/courses', async (req, res) => {
+
+      const course = req.body;
+
+      const result = await courseCollection.insertOne(course)
+
+      res.send(result)
+    })
+
+    app.patch('/courses/:id', async (req, res) => {
+      const id = req.params.id;
+
+      const filter = { _id: new ObjectId(id) }
+
+      const updatedDoc = {
+
+        $set: {
+
+          status: "Approved"
+
+        }
+      }
+
+      const result = await courseCollection.updateOne(filter, updatedDoc)
+
+      res.send(result)
+    })
+    app.patch('/courses/reject/:id', async (req, res) => {
+      const id = req.params.id;
+
+      const filter = { _id: new ObjectId(id) }
+
+      const updatedDoc = {
+
+        $set: {
+
+          status: "Rejected"
+
+        }
+      }
+
+      const result = await courseCollection.updateOne(filter, updatedDoc)
+
+      res.send(result)
+    })
+
+    // ! Confirmed Course API
+
+    app.get("/confirmedCourse", async (req, res) => {
+      const result = await ConfirmedCourseCollection.find().toArray()
+      res.send(result)
+    })
+
+    app.post('/confirmedCourse', async (req, res) => {
+
+      const course = req.body;
+
+      console.log(req.body)
+
+      const result = await ConfirmedCourseCollection.insertOne(course);
+
+      res.send(result)
+    })
+
+    app.delete('/confirmedCourse/:title', async (req, res) => {
+
+      const title = req.params.title;
+
+      const query = { title: title };
+
+      const isCourse = await ConfirmedCourseCollection.findOne(query);
+
+      if (!isCourse) {
+        return res.send({ message: "Course not found" })
+      }
+
+      const result = await ConfirmedCourseCollection.deleteOne(query);
 
       res.send(result)
     })
@@ -222,13 +321,15 @@ async function run() {
     app.get('/courses/:id', async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
-      const result = await courseCollection.findOne(query);
+      const result = await ConfirmedCourseCollection.findOne(query);
       res.send(result)
     })
 
 
+
+
     app.get('/learnToday', async (req, res) => {
-      const result = await courseCollection.find()
+      const result = await ConfirmedCourseCollection.find()
         .sort({ totalEnrollment: -1 })
         .limit(4)
         .toArray();
